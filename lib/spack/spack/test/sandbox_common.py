73fc68cc735827a4ab2d29ca39dca73fd4d09426
Bernhard Kaindl <bernhardkaindl7@gmail.com>
2026-09-26 12:06:11 +0200

    sandbox: select expr for source configuration

--- lib/spack/spack/test/sandbox_common.py
+++ lib/spack/spack/test/sandbox_common.py
@@ -62,6 +62,7 @@ def test_namespace_policy_data_is_loaded_from_yaml():
     assert policy["device_symlinks"]["/dev/fd"] == "/proc/self/fd"
     assert "tar" in policy["stage_programs"]
     assert "chmod" in policy["stage_programs"]
+    assert "expr" in policy["stage_programs"]
     assert "sed" in policy["stage_programs"]
     assert header_policy["version"] == 1
     assert header_policy["system_include_root"] == "/usr/include"
